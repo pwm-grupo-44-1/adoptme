acd6447daa765af061bd2eea10f435c5f00890b6
Esther viera <esthervieriv@gmail.com>
2026-04-16 16:37:29 +0100

    - Stories migrado a angular

--- angular-version/src/app/app.routes.ts
+++ angular-version/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import {AdoptionList} from './pages/adoption-list/adoption-list';
-import {PetProfile} from './pages/pet-profile/pet-profile';
-import {Home} from './pages/home/home';
+import { AdoptionList } from './pages/adoption-list/adoption-list';
+import { PetProfile } from './pages/pet-profile/pet-profile';
+import { Home } from './pages/home/home';
+import { Stories } from './pages/stories/stories';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'adoption-list', component: AdoptionList },
   { path: 'pet-profile', component: PetProfile },
-  { path: '**', redirectTo: '' },
-  { path: '', component: Home }
+  { path: 'stories', component: Stories },
+  { path: '**', redirectTo: '' }
 ];
--- angular-version/src/app/pages/stories/stories.ts
+++ angular-version/src/app/pages/stories/stories.ts
@@ -1,9 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface StoryItem {
+  name: string;
+  text: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-stories',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './stories.html',
-  styleUrl: './stories.css',
+  styleUrl: './stories.css'
 })
-export class Stories {}
+export class Stories {
+  stories: StoryItem[] = [
+    {
+      name: 'Toby',
+      text: 'Toby llegó a nosotros asustado y desnutrido. Hoy vive con la familia Rodríguez y es el perro más feliz del barrio.',
+      image: '/img/stories/toby.jpg'
+    },
+    {
+      name: 'Luna',
+      text: 'Luna fue encontrada en la calle con solo dos semanas de vida. Tras meses de cuidados, encontró su hogar para siempre.',
+      image: '/img/stories/luna.jpg'
+    },
+    {
+      name: 'Rocky',
+      text: 'Rocky estuvo tres años esperando. Nadie lo elegía por su tamaño. Hoy tiene jardín y dos niños que lo adoran.',
+      image: '/img/stories/rocky.jpg'
+    },
+    {
+      name: 'Nala',
+      text: 'Nala llegó con miedo a las personas. Con paciencia y amor, se convirtió en la gata más cariñosa que puedas imaginar.',
+      image: '/img/stories/nala.jpg'
+    }
+  ];
+}
