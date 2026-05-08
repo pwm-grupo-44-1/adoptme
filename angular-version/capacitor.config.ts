f49aa9b85487d363173fbb1a870fd35dd05b6365
Alejandro Hernández <alejandro.hdelgado144@gmail.com>
2026-05-08 14:33:43 +0100

    - Añadida las dependecias de ionic

---
+++ angular-version/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.adoptme.app',
+  appName: 'AdoptMe',
+  webDir: 'dist/angular-version/browser'
+};
+
+export default config;
